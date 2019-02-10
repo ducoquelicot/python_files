23e4add0865b263a72728f27fcfd94855b6bc2f4
Fabienne <fmeijer@stanford.edu>
2019-02-10 13:25:55 -0800

    ok

--- read_txt.py
+++ read_txt.py
@@ -5,9 +5,9 @@ def main():
     text_list = glob.glob('/home/fabienne/Desktop/Python/Letters/letters_pdfs_2019*.txt')
 
     for text in text_list:
-        keyword_search(text)
+        keyword = keyword_search(text)
 
-    email = "you might want to take a look at the following {} files that contain the 'Hotel President' keyword:\n\n".format(len(key_files))
+    email = "you might want to take a look at the following {} files that contain the requested keyword {}:\n\n".format(len(key_files), keyword)
 
     for keyfile in key_files:
         filename = os.path.basename(keyfile)[:-4]
@@ -18,7 +18,7 @@ def main():
     smtpObj.ehlo()
     smtpObj.starttls()
     smtpObj.login('fabienne.rosina.nicole@gmail.com', os.environ['dev_pass'])
-    smtpObj.sendmail('fabienne.rosina.nicole@gmail.com', 'fmeijer@stanford.edu', 'Subject: Letters to City Council that mention Hotel President\nHi Fabienne, {}'.format(email))
+    smtpObj.sendmail('fabienne.rosina.nicole@gmail.com', 'fmeijer@stanford.edu', 'Subject: {} Letters that mention {}\nHi Fabienne, {}'.format(len(key_files), keyword, email))
     smtpObj.quit()
 
 def keyword_search(text_path):
@@ -26,10 +26,12 @@ def keyword_search(text_path):
     filename = os.path.basename(text_path)[:-4]
     with open(text_path) as council_letters: 
         contents = council_letters.read()
-        if 'Hotel President' in contents:
+        keyword = 'President Hotel'
+        if keyword in contents:
             key_files.append(text_path)
         else:
             print('Keyword not found in ' +filename)
+    return keyword
 
 if __name__ == '__main__':
     main()
